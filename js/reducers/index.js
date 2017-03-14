1dedff72bfca1b294286d00d1e272f5adb0cb849
chathuranga94 <chathuranga94@live.com>
2017-03-15 01:29:13 +0530

    async data fetch to store

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -4,10 +4,11 @@ import { combineReducers } from 'redux';
 import drawer from './drawer';
 import routes from './routes';
 import cardNavigation from './cardNavigation';
+import apiReducer from './apiReducer';
 
 export default combineReducers({
-
   drawer,
   cardNavigation,
-  routes
+  routes,
+  data:apiReducer
 });
