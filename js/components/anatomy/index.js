5d9470b1abf042c64847bf38d464c40211ff880f
chathuranga94 <chathuranga94@live.com>
2017-03-15 02:48:03 +0530

    repeat component

--- js/components/anatomy/index.js
+++ js/components/anatomy/index.js
@@ -1,19 +1,23 @@
 
 import React, { Component } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Image, View} from 'react-native';
 import { connect } from 'react-redux';
-import { Container, Header, Title, Content, Text, H3, Button, Icon, Footer, FooterTab, Left, Right, Body } from 'native-base';
+import { Container, Header, Title,Content, Text, H3, Button, Icon, Footer, FooterTab, Left, Right, Body, Card, CardItem, Thumbnail} from 'native-base';
 
 import { openDrawer } from '../../actions/drawer';
 import styles from './styles';
+
+const cardImage = require('../../../img/drawer-cover.png');
 
 class Anatomy extends Component {
 
   static propTypes = {
     openDrawer: React.PropTypes.func,
+    photos: React.PropTypes.array,
   }
 
   render() {
+
     return (
       <Container style={styles.container}>
         <Header>
@@ -23,25 +27,43 @@ class Anatomy extends Component {
             </Button>
           </Left>
           <Body>
-            <Title>Header</Title>
+            <Title>PHOTOS</Title>
           </Body>
           <Right />
-
         </Header>
 
-
         <Content padder>
-          <Text>
-            Content Goes Here
-          </Text>
+
+          {this.props.photos.slice(20,30).map(photo => {
+            return(
+              <Card style={styles.mb} key={photo.id}>
+                <CardItem cardBody>
+                  <Image style={{ resizeMode: 'cover', width: null, height: 200, flex: 1 }} source={photo.url} />
+              </CardItem>
+                <CardItem style={{ paddingVertical: 0 }}>
+                  <Left>
+                    <Button iconLeft transparent>
+                      <Icon active name="thumbs-up" />
+                      <Text> {photo.id} Likes</Text>
+                    </Button>
+                  </Left>
+                  <Right>
+                    <Button iconLeft transparent>
+                      <Icon active name="chatbubbles" />
+                      <Text> {photo.albumId} Comments</Text>
+                    </Button>
+                  </Right>
+                </CardItem>
+              </Card>
+            );
+          })}
 
         </Content>
-
 
         <Footer>
           <FooterTab>
             <Button active full>
-              <Text>Footer</Text>
+              <Text>FOOTER : {this.props.photos[60].id}</Text>
             </Button>
           </FooterTab>
         </Footer>
@@ -59,6 +81,7 @@ function bindAction(dispatch) {
 const mapStateToProps = state => ({
   navigation: state.cardNavigation,
   themeState: state.drawer.themeState,
+  photos: state.data.photos,
 });
 
 export default connect(mapStateToProps, bindAction)(Anatomy);
