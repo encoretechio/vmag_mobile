c74d04891848ffae203d50046a95057d0e4007c4
Janaka Chathuranga <bjchathuranga@gmail.com>
2017-03-16 01:11:49 +0530

    Added data fetching

--- js/components/login/index.js
+++ js/components/login/index.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { Image, View, StatusBar } from 'react-native';
-import { connect } from 'react-redux';
-import { Container, Button, H3, Text } from 'native-base';
+import React, {Component} from 'react';
+import {Image, View, StatusBar} from 'react-native';
+import {connect} from 'react-redux';
+import {Container, Button, H3, Text} from 'native-base';
 
-import { openDrawer } from '../../actions/drawer';
+import {openDrawer} from '../../actions/drawer';
+import {login} from '../../actions/api';
 import styles from './styles';
 
 const launchscreenBg = require('../../../img/background.jpg');
@@ -21,7 +22,7 @@ class Home extends Component { // eslint-disable-line
         <StatusBar barStyle='light-content'/>
         <Image source={launchscreenBg} style={styles.imageContainer}>
           <View style={styles.logoContainer}>
-            <Image source={launchscreenLogo} style={styles.logo} />
+            <Image source={launchscreenLogo} style={styles.logo}/>
           </View>
           <View style={{ alignItems: 'center', marginBottom: 50, backgroundColor: 'transparent'}}>
             {/*<H3 style={styles.text}>Welcome to</H3>*/}
@@ -29,9 +30,17 @@ class Home extends Component { // eslint-disable-line
             <H3 style={styles.text}>Login</H3>
           </View>
           <View style={{ marginBottom: 80 }}>
+            <Text>{this.props.playlistCount}</Text>
             <Button
               style={{ backgroundColor: '#000099', alignSelf: 'center' }}
               onPress={this.props.openDrawer}
+            >
+              <Text>Open Drover</Text>
+            </Button>
+            <Text>{this.props.token}</Text>
+            <Button
+              style={{ backgroundColor: '#000099', alignSelf: 'center' }}
+              onPress={this.props.login}
             >
               <Text>Login</Text>
             </Button>
@@ -45,6 +54,7 @@ class Home extends Component { // eslint-disable-line
 function bindActions(dispatch) {
   return {
     openDrawer: () => dispatch(openDrawer()),
+    login: () => dispatch(login()),
   };
 }
 
@@ -52,6 +62,8 @@ const mapStateToProps = state => ({
   navigation: state.cardNavigation,
   themeState: state.drawer.themeState,
   routes: state.drawer.routes,
+  token: state.data.token,
+  playlistCount: state.data.userData?state.data.userData.playlists.length:0
 });
 
 export default connect(mapStateToProps, bindActions)(Home);
