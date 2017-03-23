fa89d7f0106f10667dfa1d404f6cf78034716446
Janaka Chathuranga <bjchathuranga@gmail.com>
2017-03-23 12:14:36 +0530

    Bind data, comment video tag

--- js/reducers/apiReducer.js
+++ js/reducers/apiReducer.js
@@ -1,4 +1,4 @@
-import {GET_POSTS, GET_PHOTOS, LOGIN_SUCCESS, SET_USER_DATA} from '../actions/api';
+import {GET_POSTS, GET_PHOTOS, LOGIN_SUCCESS, LOGIN_FAIL, CONNECTION_ERROR, SET_FINAL_ISSUE} from '../actions/api';
 
 import sampleData from './sampleData.json';
 
@@ -14,34 +14,58 @@ const initialState = {
 
 export default function apiReducer(state: State = initialState, action) {
 
+  // console.log(action);
+  // console.log("______________________________________");
+  // console.log(state);
+  // console.log("______________________________________");
+  // console.log("______________________________________");
   if (action.type === GET_POSTS) {
     return {
       ...state,
-      posts: action.data,
-      company: sampleData.company,
-      cover: sampleData.cover,
-      playlists: sampleData.playlists
+      // posts: action.data,
+      // company: sampleData.company,
+      // cover: sampleData.cover,
+      // playlists: sampleData.playlists
     };
   }
 
   if (action.type === GET_PHOTOS) {
     return {
       ...state,
-      photos: action.data,
+      // photos: action.data,
     };
   }
   if (action.type === LOGIN_SUCCESS) {
     console.log(action.data.token)
     return {
       ...state,
+      connectionError:false,
+      loginError:false,
       user: action.data.user,
       token: action.data.token
     }
   }
-  if(action.type === SET_USER_DATA) {
+  if (action.type === LOGIN_FAIL) {
+    console.log(action.data.token)
     return {
       ...state,
-      userData:action.data
+      loginError:true
+    }
+  }
+  if (action.type === CONNECTION_ERROR) {
+    console.log(action.data.token)
+    return {
+      ...state,
+      connectionError:true
+    }
+  }
+  if(action.type === SET_FINAL_ISSUE) {
+    return {
+      ...state,
+      finalIssue:action.data,
+      cover:action.data.cover_video,
+      playlists:action.data.playlists,
+      connectionError:false,
     }
   }
 
